23fac95cceae21f4ba3c7fb85f07f3f96d686052
aksharpat <120274494+aksharpat@users.noreply.github.com>
2023-03-31 10:23:33 -0500

    Finger values

---
+++ individualFingers.ino
@@ -0,0 +1,65 @@
+#include <ESP32Servo.h>
+/* Include the HCPCA9685 library */
+#include "HCPCA9685.h"
+//#include "Servo.h" 
+/* I2C slave address for the device/module. For the HCMODU0097 the default I2C address
+   is 0x40 */
+#define  I2CAdd 0x40
+ 
+Servo thumb_servo;
+/*
+Start Posistion Angle:
+Max Contract Angle:
+Max Expand Angle:
+*/
+Servo index_servo;
+/*
+Start Posistion Angle:268
+Max Contract Angle:10
+Max Expand Angle:268
+*/
+Servo middle_servo;
+/*
+Start Posistion Angle:285
+Max Contract Angle:40
+Max Expand Angle:285
+*/
+Servo ring_servo;
+/*
+Start Posistion Angle:55
+Max Contract Angle:300
+Max Expand Angle:55
+*/
+Servo pinkie_servo;
+/*
+Start Posistion Angle:100
+Max Contract Angle:340
+Max Expand Angle:100
+*/
+
+/* Create an instance of the library */
+HCPCA9685 HCPCA9685(I2CAdd);
+int f = 0;
+ 
+void setup() 
+{
+  /* Initialise the library and set it to 'servo mode' */ 
+  HCPCA9685.Init(SERVO_MODE);
+ //HCPCA9685.Servo(2, 100);
+  /* Wake the device up */
+  HCPCA9685.Sleep(false);
+}
+ 
+ 
+void loop() 
+{
+  delay(2000);
+  HCPCA9685.Servo(4, 330);
+  HCPCA9685.Servo(5, 100);
+
+  delay(1500);
+  HCPCA9685.Servo(4, 100);
+  HCPCA9685.Servo(5, 250);
+
+
+}
